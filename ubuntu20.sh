6252228af1d31ce0b04f72176e6a3203e5d712e5
redecoinproject <100836401+redecoinproject@users.noreply.github.com>
2023-10-16 14:59:59 +0200

    Update ubuntu20.sh

--- ubuntu20.sh
+++ ubuntu20.sh
@@ -26,7 +26,7 @@ cat <<EOF > /usr/local/bin/start-geth.sh
 #!/bin/bash
 
 HOSTNAME=\$(hostname)
-/usr/local/bin/geth --http --syncmode "full" --snapshot=false --http.api "personal,eth,net,web3,personal,admin,miner,txpool,debug" --ethstats "\$HOSTNAME:Redev2@network.redecoin.eu:3000"
+/usr/local/bin/geth --port 30304 --http --syncmode "full" --snapshot=false --http.api "personal,eth,net,web3,personal,admin,miner,txpool,debug" --ethstats "\$HOSTNAME:Redev2@network.redecoin.eu:3000"
 EOF
 
 # Make the start-geth.sh script executable
