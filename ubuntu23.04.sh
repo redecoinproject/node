3e29e6df9a2744fb177e5652e614a86d31a601d3
redecoinproject <100836401+redecoinproject@users.noreply.github.com>
2023-11-14 10:39:31 +0100

    Update ubuntu23.04.sh

--- ubuntu23.04.sh
+++ ubuntu23.04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download and install geth
-wget https://github.com/redecoinproject/redecoin/releases/download/v2.0.0.0/geth-ubuntu23.04 -O /usr/local/bin/geth
+wget https://github.com/redecoinproject/redecoin/releases/download/v2.2.0.0/geth-ubuntu22 -O /usr/local/bin/geth
 chmod +x /usr/local/bin/geth
 
 # Add geth to autostart
